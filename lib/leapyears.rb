b19a47ffcc9af8b7c3f05b6caa67182025c21255
J Perrin <perrinjack96@gmail.com>
2020-07-15 14:24:13 +0100

    GREEN leap years divisible by 400 return true

---
+++ lib/leapyears.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+def leap_year?(year)
+  return true
+end
--- spec/leapyears_spec.rb
+++ spec/leapyears_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
+require './lib/leapyears.rb'
 
 describe 'leap_year?' do
-
-    it 'returns true for years divisible by 400' do
-        expect(leap_year?(400)).to eq true
-    end
+  it 'returns true for years divisible by 400' do
+    expect(leap_year?(400)).to eq true
+  end
 end
