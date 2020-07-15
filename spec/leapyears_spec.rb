5c689bcd6659a56bccccd304357dacb6ad0672c6
J Perrin <perrinjack96@gmail.com>
2020-07-15 14:27:58 +0100

    RED returns false for years divisble by 100 but not by 400

--- spec/leapyears_spec.rb
+++ spec/leapyears_spec.rb
@@ -6,4 +6,8 @@ describe 'leap_year?' do
   it 'returns true for years divisible by 400' do
     expect(leap_year?(400)).to eq true
   end
+
+  it 'returns false for years divisible by 100 but not by 400' do
+    expect(leap_year?(300)).to eq false
+  end
 end
